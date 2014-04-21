832ffa140103ae4898895a64cd76a40cbe84b62e
Graeme Coupar <grambo@grambo.me.uk>
2014-04-21 01:07:04 +0100

    Updated main supervisor w/ rest of children.

--- lib/pyexq/supervisor.ex
+++ lib/pyexq/supervisor.ex
@@ -17,12 +17,18 @@ defmodule Pyexq.Supervisor do
     ]
 
     children = [
-      func_supervisor(:delete_sup, &Pyexq.GoogleAPI.delete_task/1, restart: :transient),
       supervisor(Pyexq.LeaseHolderSupervisor, []),
-
-      :poolboy.child_spec(:python_pool, pool_options, []),
+      func_supervisor(:delete_sup, &Pyexq.GoogleAPI.delete_task/1),
+      func_supervisor(:release_sup, &Pyexq.GoogleAPI.release_lease/1),
 
       worker(Pyexq.TokenHandler, []),
+      worker(Pyexq.Leaser, []),
+
+      worker(Pyexq.Worker, [], id: 'worker1'),
+      worker(Pyexq.Worker, [], id: 'worker2'),
+      worker(Pyexq.Worker, [], id: 'worker3'),
+      worker(Pyexq.Worker, [], id: 'worker4'),
+      :poolboy.child_spec(:python_pool, pool_options, []),
     ]
 
     # See http://elixir-lang.org/docs/stable/Supervisor.Behaviour.html
@@ -30,7 +36,7 @@ defmodule Pyexq.Supervisor do
     supervise(children, strategy: :one_for_one)
   end
 
-  defp func_supervisor(id, function, args) do
-    supervisor(Pyexq.FunctionSupervisor, [id, function, args], id: id)
+  defp func_supervisor(id, function) do
+    supervisor(Pyexq.FunctionSupervisor, [id, function, [{:restart, :transient}]], id: id)
   end
 end
