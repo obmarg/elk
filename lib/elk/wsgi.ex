f5284f9637d32a5cae94e17898f3d0031dd0129d
Graeme Coupar <grambo@grambo.me.uk>
2014-04-22 22:29:48 +0100

    WSGI now supports passing data in to tasks
    Switched to using WebOb to help with WSGI - something wasn't working
    right with my WSGI implementation, and why bother fixing it when there's
    a perfectly good library I can use.

--- lib/elk/wsgi.ex
+++ lib/elk/wsgi.ex
@@ -4,44 +4,21 @@ defmodule Elk.WSGI do
   '''
   require Lager
 
-  # For documentation on WSGI parameters:
-  # http://wsgi.readthedocs.org/en/latest/definitions.html
-  @global_wsgi_params [{"wsgi.version", {1, 0}},
-                       {"wsgi.url_scheme", "http"},
-                       {"wsgi.multithread", false},
-                       {"wsgi.multiprocess", true},
-                       {"wsgi.run_once", false},
-                       {"SERVER_SOFTWARE", "Elk"},
-                       {"SERVER_NAME", "0.0.0.0"},
-                       {"GATEWAY_INTERFACE", "CGI/1.1"},
-                       {"SERVER_PROTOCOL", "HTTP/1.1"},
-                       {"SERVER_PORT", "80"},
-                       {"REQUEST_METHOD", "POST"},
-                       {"REMOTE_ADDR", "0.0.0.0"},
-                       {"SCRIPT_NAME", ""},
-                       {"CONTENT_TYPE", "application/json"}]
-
   def call_task(py_worker, module_name, app_name, task) do
     call_app(py_worker, module_name, app_name, task.url, task.payload)
   end
 
   def call_app(py_worker, module_name, app_name, url, input) do
-    wsgi_params = Dict.put(@global_wsgi_params, "PATH_INFO", url)
-    params = [ module_name, app_name, wsgi_params, input ]
-    Lager.info "PLEASE"
-    Lager.info input
-
-    {status, headers, body, errors} = :python.call(
+    params = [ module_name, app_name, url, "POST", input ]
+    {status, headers, body} = :python.call(
       py_worker, :wsgi_wrapper, :call_application, params
     )
     # TODO: Need to check for non-200 returns in here...
     Lager.info "Worker Finished: #{status}"
     Lager.info "Body: #{body}"
-    Lager.info errors
     nil
 
-    # TODO: Need to do something for wsgi.input & wsgi.errors
-    # TODO: also need some timeout stuff & tests.
+    # TODO: Need some timeout stuff & tests.
   end
 
 end
--- wsgi_wrapper/__init__.py
+++ wsgi_wrapper/__init__.py
@@ -1,5 +1,6 @@
 import importlib
 from cStringIO import StringIO
+from webob import Request
 
 def get_application(module_name, app_name):
     '''
@@ -12,47 +13,28 @@ def get_application(module_name, app_name):
     return getattr(module, app_name)
 
 
-def call_application(module_name, app_name, environ, input_str):
+def call_application(module_name, app_name, url, method, input_str):
     '''
     Wrapper around calling a WSGI application
 
     :param module_name: The module to import
     :param app_name:    The name of the application within the module.
-    :param environ:     A list of tuples, representing the pairs of the environ
-                        dict.
+    :param url:         The url to send things to
+    :param method:      The HTTP method
     :param input_str:   The string to use for an input stream.
 
     :returns:           A tuple of (status, headers, body, error_stream)
     '''
-    body = []
-    status_headers = [None, None]
-
-    def start_response(status, headers):
-        status_headers[:] = [status, headers]
-
-    environ = dict(environ)
-    # TODO: Tried wsgiref.util.FileWrapper.
-    #       Would have been nice if it worked...
-    #       Think gunicorn might be a good place to look for an example.
-    #       Looks like gunicorn.http.body.Body is what would be passed in
-    #       as wsgi.input there.
-    #       If that doesn't help, then maybe should implement my own class
-    #       with logging to see how stuff is being dealt with...
-    environ['wsgi.input'] = StringIO(input_str)
-    environ['wsgi.error'] = err_stream = StringIO()
+    req = Request.blank(url)
+    req.method = method
+    req.body = input_str
+    req.content_type = 'application/json'
 
     app = get_application(module_name, app_name)
-    app_iter = app(environ, start_response)
+    response = req.get_response(app)
 
-    try:
-        for item in app_iter:
-            body.append(item)
-    finally:
-        if hasattr(app_iter, 'close'):
-            app_iter.close()
-
-    return (status_headers[0], status_headers[1], ''.join(body),
-            err_stream.getvalue())
+    # TODO: Need a way to get err_stream from this.
+    return (response.status_code, response.headerlist, response.body)
 
 
 def _test_app(environ, start_response):
