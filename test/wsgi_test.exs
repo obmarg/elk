c3f340aed180e7ad5e3c160f7aefa7efceb6cfd1
Graeme Coupar <grambo@grambo.me.uk>
2014-05-15 23:25:25 +0100

    Tests of Elk.WSGI.call_task

--- test/wsgi_test.exs
+++ test/wsgi_test.exs
@@ -1,10 +1,6 @@
 defmodule WsgiTest do
   use ExUnit.Case
 
-  defp call_app(context, app, input \\ "") do
-    Elk.WSGI.call_app(context[:worker], "wsgi_wrapper.test", app, "", input)
-  end
-
   setup do
     {:ok, worker} = :python.start(Elk.Python.python_config)
     {:ok, worker: worker}
@@ -15,11 +11,28 @@ defmodule WsgiTest do
     :ok
   end
 
-  test "call_app handles simple 200", context do
+  test "call_app on on 200", context do
     assert call_app(context, "return200") == :ok
   end
 
   test "call_app errors on 500", context do
     assert call_app(context, "return500") == :task_failed
+  end
+
+  test "call_task ok on 200", context do
+    assert call_task(context, "return200") == :ok
+  end
+
+  test "call_task errors on 500", context do
+    assert call_app(context, "return500") == :task_failed
+  end
+
+  defp call_app(context, app, input \\ "") do
+    Elk.WSGI.call_app(context[:worker], "wsgi_wrapper.test", app, "", input)
+  end
+
+  defp call_task(context, app, input \\ "") do
+    task = Elk.Task[url: "", payload: ""]
+    Elk.WSGI.call_task(context[:worker], "wsgi_wrapper.test", app, task)
   end
 end
