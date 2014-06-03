da6f0d13e1e1848f817a15f3ad7a20ad4b12bcfc
Graeme Coupar <grambo@grambo.me.uk>
2014-06-03 22:30:44 +0100

    WIP

--- test/token_handler_test.ex
+++ test/token_handler_test.ex
@@ -8,12 +8,21 @@ defmodule TokenHandlerFacts do
     #       Mocking doesn't seem to work
 
     it "requests token on first request" do
+      response = HTTPotion.Response[body: "", status_code: 200, headers: {}]
+      provided [HTTPotion.post |> response] do
+        Elk.TokenHandler.handle_call(:get_token, nil, nil) |> { :reply, _, _ }
+      end
     end
 
     it "uses cache on second request" do
+      assert false == true
     end
 
     it "re-requests token on expiry" do
+    end
+
+    it "errors on non 200" do
+
     end
 
   end
