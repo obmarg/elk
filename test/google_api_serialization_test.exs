6e8eca33e5afae9cc096affe434d0c04ba569a75
Graeme Coupar <grambo@grambo.me.uk>
2014-06-04 18:58:40 +0100

    Added support for gzipped data to GoogleAPIReader.
    The app engine task queue seems to have a wierd bug.  In certain
    circumstances it will send totally corrupted data to the client - data
    that is missing padding, and only some of the existing decoded data
    makes sense.  From my limited testing, it seems that this is triggered
    by ?s in the data, since it's always ?s that start the corrupted data,
    and removing them causes success.
    
    Since I can't change appengine, and removing ?s from all data is
    unrealistic, I'm adding a gzipped mode to Elk.  This mode will accept
    gzipped, base64 encoded data in the "payload" parameter.  Base64
    encoding doesn't have ?s, so we should be safe with this mode.

--- test/google_api_serialization_test.exs
+++ test/google_api_serialization_test.exs
@@ -1,18 +1,8 @@
 defmodule GoogleAPIReaderTest do
   use ExUnit.Case
 
-  def make_task_data(payload) do
-    json_data = HashDict.new(url: "/",
-                             payload: payload)
-                |> JSON.encode!
-
-    HashDict.new([{"id", 123},
-                  {"retry_count", 1},
-                  {"payloadBase64", :base64.encode(json_data)}])
-  end
-
   test "reads normal data" do
-    data = make_task_data([1, 2, 3])
+    data = make_task_data([1, 2, 3], false)
     result = GoogleAPIReader.parse_task(data) 
     
     assert result == Elk.Task[id: 123,
@@ -20,6 +10,35 @@ defmodule GoogleAPIReaderTest do
                               payload: "[1,2,3]",
                               orig: data,
                               retries: 1]
+  end
+
+  test "reads gzipped data" do
+    data = [1, 2, 3]
+           |> compress_data
+           |> make_task_data
+
+    result = GoogleAPIReader.parse_task(data)
+
+    assert result == Elk.Task[id: 123,
+                              url: "/",
+                              payload: "[1,2,3]",
+                              orig: data,
+                              retries: 1]
+  end
+
+  defp make_task_data(payload, gzip // true) do
+    json_data = HashDict.new(url: "/",
+                             payload: payload,
+                             gzip: gzip)
+                |> JSON.encode!
+
+    HashDict.new([{"id", 123},
+                  {"retry_count", 1},
+                  {"payloadBase64", :base64.encode(json_data)}])
+  end
+
+  defp compress_data(data) do
+    data |> JSON.encode! |> :zlib.compress |> :base64.encode
   end
 
 end
